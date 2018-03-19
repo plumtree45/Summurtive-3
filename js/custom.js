2cfadf14708d7423049e1dfd83ccee5c25fb619f
Lewis Plumley <845859554@bun2-206-19.local>
2018-03-20 12:17:13 +1300

    adding additional files for the commit to be finshed

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$(".button5").click(function() {
+		$(".form").show();
+	});
+
+	$("#submit").click(function() {
+		$(".form").hide();
+	});
+});
+
+
+
+// if (document.addEventListen) {
+// 	document.addEventListen(".button5", myFunction);
+// } else if () {}
+
+
+
+
+
+
+
+
+
+// .addEventListen(".button5")
